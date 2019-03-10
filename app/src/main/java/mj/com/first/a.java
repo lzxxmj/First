3a621a71f05fc459483c6159e8c0bda54732876c
mj <majie880103@sina.com>
2019-03-11 00:16:47 +0800

    a类中添加了变量 c 修改了ff方法

--- app/src/main/java/mj/com/first/a.java
+++ app/src/main/java/mj/com/first/a.java
@@ -1,8 +1,18 @@
 package mj.com.first;
 
+import android.util.Log;
+
 public class a {
+
     int a;
     int b;
 
+
+
+    int c;
+    public void ff(){
+
+        Log.i("aaa","hahahhah");
+    }
 
 }
